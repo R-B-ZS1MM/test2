2364eaaa56242141ac17651e98b5ed4dd4888ab9
Radoslaw Biereta <radoslaw.biereta@live.zs1mm.edu.pl>
2024-07-21 22:10:26 +0200

    zmiany

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-StylePropertyMap.css
+function changeColor() {
+    // document.getElementsByTagName('body')[0].style.backgroundColor = 'red';
+    document.querySelector('body').style.backgroundColor = 'red';
+}
+
